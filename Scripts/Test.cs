e7d4b02d184958d2e7466ec265b6d3ce7a36aa39
Ilya <arzaevilya@gmail.com>
2021-06-04 20:57:47 +0200

    hugeOk

--- Scripts/Test.cs
+++ Scripts/Test.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.SceneManagement;
 
 public class Test : MonoBehaviour
 {
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3f);
+        //Destroy(gameObject, 3f);
+
+        rb = GetComponent<Rigidbody>();
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.AddForce(Vector3.up * 500);
+
+        }
+
+        //rb.velocity = Vector3.forward * 20f;
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
     }
 }
